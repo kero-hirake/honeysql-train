8c18c2946525831c7d834d6f928de222177a7b18
kero_hirake <k16.h.white@gmail.com>
2021-12-01 13:13:18 +0900

    lein new app

---
+++ test/honeysql_practice/core_test.clj
@@ -0,0 +1,7 @@
+(ns honeysql-practice.core-test
+  (:require [clojure.test :refer :all]
+            [honeysql-practice.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
